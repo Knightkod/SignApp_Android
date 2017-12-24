dfb57c8fd60f77bddb74b6b50fdabf291111024f
Knightkod <pfernandez046@ikasle.ehu.eus>
2017-12-24 09:23:25 +0100

    Commit inicial de la aplicación SignApp. Creación del proyecto sin añadir cambios.

---
+++ SignApp/app/src/main/java/eus/ehu/intel/signapp/MainActivity.java
@@ -0,0 +1,13 @@
+package eus.ehu.intel.signapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
