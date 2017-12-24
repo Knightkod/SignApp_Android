6cf3adef14eb103d5ea35ff9da917010083ae180
Knightkod <pfernandez046@ikasle.ehu.eus>
2017-12-24 15:23:24 +0100

    Añadidas UI de Informacion general y página de Traductor

---
+++ SignApp/app/src/main/java/eus/ehu/intel/signapp/TraductorActivity.java
@@ -0,0 +1,13 @@
+package eus.ehu.intel.signapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TraductorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_traductor);
+    }
+}
