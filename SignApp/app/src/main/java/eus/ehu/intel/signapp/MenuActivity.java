7bc4e72515f2f2a1a42d4f6ed6eff9c1dbadf19e
Knightkod <pfernandez046@ikasle.ehu.eus>
2017-12-24 19:11:36 +0100

    Añadida funcionalidad de cambio entre páginas para MainActivity, MenuActivity.

--- SignApp/app/src/main/java/eus/ehu/intel/signapp/MenuActivity.java
+++ SignApp/app/src/main/java/eus/ehu/intel/signapp/MenuActivity.java
@@ -1,7 +1,10 @@
 package eus.ehu.intel.signapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void toGeninfo(View view){
+        Intent intent = new Intent(this,GeninfoActivity.class);
+        startActivity(intent);
+    }
+    public void toTraductor(View view){
+        Intent intent = new Intent(this,TraductorActivity.class);
+        startActivity(intent);
+    }
+    public void toAlphab(View view){
+        Intent intent = new Intent(this,AlphabetActivity.class);
+        startActivity(intent);
+    }
+    public void toForum(View view){
+        Toast.makeText(this,"Funcion aun no disponible",Toast.LENGTH_SHORT).show();
+    }
+    public void recordVideo(View view){
+        Toast.makeText(this,"Funcion aun no disponible",Toast.LENGTH_SHORT).show();
     }
 }
