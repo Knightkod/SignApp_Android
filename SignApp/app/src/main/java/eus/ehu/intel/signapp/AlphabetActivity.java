9e81cfcc210117b5f2891b390bd5c1472027e132
Knightkod <pfernandez046@ikasle.ehu.eus>
2017-12-24 19:13:34 +0100

    Añadida funcionalidad a AlphabetActivity para rellenar el layout con las fotos de letra  del abecedario contenidas en la carpeta res/drawable

---
+++ SignApp/app/src/main/java/eus/ehu/intel/signapp/AlphabetActivity.java
@@ -0,0 +1,35 @@
+package eus.ehu.intel.signapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.Toast;
+
+public class AlphabetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alphabet);
+        insertAlphabImages();
+       }
+
+    private void insertAlphabImages(){
+        ImageView imgView;
+        String[] alph={"a","b","c","d","e","f","g","h","i",
+                "j","k","l","m","n","o","p","q","r","s","t",
+                "u","v","w","x","y","z"};
+        LinearLayout linearLayout=(LinearLayout)findViewById(R.id.alphabetScrollLinLayout);
+       LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+               LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        for(int i=0;i<alph.length;i++) {
+            imgView = new ImageView(this);
+            imgView.setImageResource(getResources().getIdentifier(alph[i],"drawable",this.getPackageName()));
+           imgView.setLayoutParams(params);
+            linearLayout.addView(imgView);
+        }
+    }
+}
