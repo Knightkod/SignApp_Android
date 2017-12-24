068f7bb90628e5d4e4c20567882c7d1a36bfe75f
Knightkod <pfernandez046@ikasle.ehu.eus>
2017-12-24 19:14:21 +0100

    Añadida la función viewVideo a TraductorActivity. De momento avisa al usuario de que la función aún no está preparada

--- SignApp/app/src/main/java/eus/ehu/intel/signapp/TraductorActivity.java
+++ SignApp/app/src/main/java/eus/ehu/intel/signapp/TraductorActivity.java
@@ -1,7 +1,10 @@
 package eus.ehu.intel.signapp;
 
+import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class TraductorActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class TraductorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_traductor);
+    }
+
+
+    public void viewVideo(View view) {
+        int buttonId = view.getId();
+        Toast.makeText(this,"Funcion aun no disponible",Toast.LENGTH_SHORT).show();
     }
 }
