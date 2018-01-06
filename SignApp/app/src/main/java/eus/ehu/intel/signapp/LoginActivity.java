1534549cc7c59a891f2f251e6915f6a9bc21d6b8
Knightkod <pfernandez046@ikasle.ehu.eus>
2018-01-06 20:34:39 +0100

    Añadida logica a LoginActivity. Los usuarios se guardan con SharedPreferences

--- SignApp/app/src/main/java/eus/ehu/intel/signapp/LoginActivity.java
+++ SignApp/app/src/main/java/eus/ehu/intel/signapp/LoginActivity.java
@@ -1,6 +1,7 @@
 package eus.ehu.intel.signapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,33 +13,57 @@ import eus.ehu.intel.signapp.Modelo.ServerConnection;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String SHARED_PREF_USERLOGIN = "userLogin";
+    private static final String SHARED_PREF_USERPASS = "userPass";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText editTextUserLogin = (EditText) findViewById(R.id.userLogin);
+        EditText editTextPasswdLogin = (EditText) findViewById(R.id.passwdLogin);
+        CheckBox chkBoxRemember = (CheckBox) findViewById(R.id.checkBoxRemindLogin);
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        String user = prefs.getString(SHARED_PREF_USERLOGIN, null);
+        String pass = prefs.getString(SHARED_PREF_USERPASS, null);
+        if (user != null)
+            editTextUserLogin.setText(user);
+        if (pass != null){
+            editTextPasswdLogin.setText(pass);
+            chkBoxRemember.setChecked(true);
+        }
     }
 
     public void login(View view) {
-        Intent intent = new Intent(this,ForumActivity.class);
-        String login = ((EditText)findViewById(R.id.userLogin)).getText().toString();
-        String passwd = ((EditText)findViewById(R.id.passwdLogin)).getText().toString();
-        CheckBox chkBoxRemember = (CheckBox)findViewById(R.id.checkBoxRemindLogin);
-        ServerConnection srvConn=new ServerConnection();
-        if(srvConn.verificaLogin(login,passwd)){
-            if(chkBoxRemember.isChecked())
-                //aqui iría el código de guardar la info
-                Toast.makeText(getApplicationContext(),"login guardado!",Toast.LENGTH_SHORT).show();
-                intent.putExtra(ForumActivity.LOGIN_ID,login);
-                intent.putExtra(ForumActivity.LOGIN_PASS,passwd);
+        Intent intent = new Intent(this, ForumActivity.class);
+        EditText editTextUserLogin = (EditText) findViewById(R.id.userLogin);
+        EditText editTextPasswdLogin = (EditText) findViewById(R.id.passwdLogin);
+        CheckBox chkBoxRemember = (CheckBox) findViewById(R.id.checkBoxRemindLogin);
+        String login = editTextUserLogin.getText().toString();
+        String passwd = editTextPasswdLogin.getText().toString();
+        ServerConnection srvConn = new ServerConnection();
+        if (srvConn.verificaLogin(login, passwd)) {
+            if (chkBoxRemember.isChecked())
+                saveLogin(login, passwd);
+            intent.putExtra(ForumActivity.LOGIN_ID, login);
+            intent.putExtra(ForumActivity.LOGIN_PASS, passwd);
             startActivity(intent);
-        }
-        else{
-            Toast.makeText(getApplicationContext(),R.string.loginRegNOK,Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), R.string.loginRegNOK, Toast.LENGTH_SHORT).show();
         }
     }
 
+    private void saveLogin(String login, String pass){
+        SharedPreferences prefs=getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(SHARED_PREF_USERLOGIN,login);
+        editor.putString(SHARED_PREF_USERPASS,pass);
+        editor.commit();
+    }
+
     public void toRegister(View view) {
-        Intent intent = new Intent(this,RegisterActivity.class);
+        Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
 
     }
